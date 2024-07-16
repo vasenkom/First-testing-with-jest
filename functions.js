905d8e9fea4b08ac7bbd2df5f0270f348d28b7cc
vasenkom <m.vasenko@yandex.ru>
2024-07-16 11:05:49 +0200

    Add a capitalize function that takes a string and returns it with the first character capitalized

---
+++ functions.js
@@ -0,0 +1,7 @@
+// A capitalize function that takes a string and returns it with the first character capitalized
+function myString(string1) {
+  let firstLetterCapitalize = string1.charAt(0).toUpperCase();
+  return firstLetterCapitalize;
+}
+
+module.exports = myString;
