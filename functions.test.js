5199d5902f61440b995af69b5c438c6dc5038825
vasenkom <m.vasenko@yandex.ru>
2024-07-16 21:58:42 +0200

    correct 2 previous tests and add a test for a calculator object that contains functions for the basic operations: add, subtract, divide, and multiply

--- functions.test.js
+++ functions.test.js
@@ -1,8 +1,24 @@
-const myStringF = require("./functions");
+const {
+  myString,
+  reversedString,
+  calculator,
+  caesarCipher,
+} = require("./functions");
 
-// A test for a capitalize function that takes a string and returns it with the first character capitalized
+// A test for a function that takes a string and returns it with the first character capitalized
 test("the first character is capitalized", () => {
-  let myString = myStringF();
-  let firstLetter = myString.charAt(0);
-  expect(firstLetter).toBe(firstLetter.toUpperCase());
+  expect(myString("hello")).toBe("Hello");
+});
+
+// A test for a reversedString function that takes a string and returns it reversed
+test("the string is reversed", () => {
+  expect(reversedString("Abcd")).toBe("dcbA");
+});
+
+// A test for a calculator object that contains functions for the basic operations: add, subtract, divide, and multiply
+test("test the calculation of calculator obj", () => {
+  expect(calculator.add(1, 2)).toBe(3);
+  expect(calculator.substract(2, 1)).toBe(1);
+  expect(calculator.multiply(2, 1)).toBe(2);
+  expect(calculator.divide(2, 1)).toBe(2);
 });
